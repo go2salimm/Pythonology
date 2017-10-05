798df42894fda894340fefbce6bdf76f357bc5ed
Salim Modak <smodak@secureauth.com>
2017-10-05 13:37:23 -0400

    using *args and **kwargs to call a function

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Pythonology
 
-Intermediate Python Learning.
-
 git clone https://github.com/go2salimm/Pythonology.git
+
+# Debugging
+pdb.set_trace()
+Commands:
+    c: continue execution
+    w: shows the context of the current line it is executing.
+    a: print the argument list of the current function
+    s: Execute the current line and stop at the first possible occasion.
+    n: Continue execution until the next line in the current function is reached or it returns.
+
+# Generators
+an iterator is an object that enables a programmer to traverse a container, particularly lists. 
+iterable -->  __iter__ or __getitem__ 
+iterator -->  next or __next__
+iteration --> looping 
+Generators are iterators.  You iterate over them once.  They generate the values on the fly and yield.
+
+# References :
+Intermediate Python Learning - http://book.pythontips.com/en/latest
+
+
+
+
+
--- args.py
+++ args.py
@@ -1,9 +1,12 @@
-# Using *args and **kwargs
-def test_var_args(f_arg, *argv):
-    print("first normal arg:" , f_arg)
+#if you want to use all three types of arguments in functions then the order is func(fargs, *args, **kwargs)
+#The most common use case is when making function decorators or modifying  code at runtime.
+# Using fargs, *args and **kwargs
+
+def test_var_args(fargs, *argv):
+    print("first normal arg:" , fargs)
     for arg in argv:
         print("another arg through *argv", arg)
-test_var_args('salim', 'python', 'cats', 'test')
+test_var_args('Salim', 'python', 'cats', 'test')
 
 
 
@@ -18,10 +21,17 @@ def test_args_kwargs(arg1, arg2, arg3):
     print("arg1: ", arg1)
     print("arg2: ", arg2)
     print("arg3: ", arg3)
-args = ("two", 3, 5)
+args = ("two", 3, 5)                                # tuple
+kwargs = {"arg3": 3, "arg2": "two", "arg1": 5}      # dictionary
+largs = [{"two":456},  (4,5), {'abc':123}]          # list
+print "*args using tuple"
 test_args_kwargs(*args)
-kwargs = {"arg3": 3, "arg2": "two", "arg1": 5}
+print "*largs using list"
+test_args_kwargs(*largs)
+print "**kwargs using dictionary"
 test_args_kwargs(**kwargs)
 
 
-# So if you want to use all three of these in functions then the order is some_func(fargs, *args, **kwargs)
+
+
+
