2ea11bfae97db518c36978dbb81d5f68d3a4c9bc
Salim Modak <smodak@secureauth.com>
2017-10-11 12:44:43 -0400

    __slots__, mutation, virtualenv

--- args.py
+++ args.py
@@ -24,12 +24,31 @@ def test_args_kwargs(arg1, arg2, arg3):
 args = ("two", 3, 5)                                # tuple
 kwargs = {"arg3": 3, "arg2": "two", "arg1": 5}      # dictionary
 largs = [{"two":456},  (4,5), {'abc':123}]          # list
+print "length of largs = " , len(largs)
+print "length of kwargs = " , len(kwargs)
 print "*args using tuple"
 test_args_kwargs(*args)
 print "*largs using list"
 test_args_kwargs(*largs)
 print "**kwargs using dictionary"
 test_args_kwargs(**kwargs)
+
+
+# whenever u call the function without the target argument,  a new list is created
+
+def add_to(element, target=None):
+    if target is None:
+        target = []
+    target.append(element)
+    return target
+
+
+print add_to(42)
+print add_to(50)
+print add_to(100, [20,30,90])
+
+
+
 
 
 
